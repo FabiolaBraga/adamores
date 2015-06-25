6ffe8bc53aff574c5d1a13c9d9f1302f2f74c58e
FabiolaBraga <fabiolabraga1991@gmail.com>
2015-06-25 11:18:10 -0300

    Alteção visual
    alteção no header, main
    e alteração pagina index.php

--- modules/adoremos/views/adoremos/index.php
+++ modules/adoremos/views/adoremos/index.php
@@ -10,10 +10,3 @@ $this->title = 'Adoremos Escola De Musica';
   </div>	
 </div>
 
-<div id="cont2">
-  Dicas
-  <div id="video">
-   <iframe width="320" height="300" src="//www.youtube.com/embed/09ykv-96pQs?list=UU44UDg23WQwTjm5cjUUmFDQ" frameborder="0" allowfullscreen></iframe>
-   <iframe width="320" height="300" src="//www.youtube.com/embed/1YJ0vJ_6QrI?list=UU44UDg23WQwTjm5cjUUmFDQ" frameborder="0" allowfullscreen></iframe>
-   <iframe width="320" height="300" src="//www.youtube.com/embed/3X5xE6Ix_cA?list=UU44UDg23WQwTjm5cjUUmFDQ" frameborder="0" allowfullscreen></iframe>
- </div>
--- modules/adoremos/views/layout/main.php
+++ modules/adoremos/views/layout/main.php
@@ -11,29 +11,33 @@
     <body>
         
         <header>
-        <div id="logo">
-          <img src="<?php echo $this->assets($theme) ?>images/logo.png" width="380" />
-        </div>
-
+              <div id="logo">
+                <img src="<?php echo $this->assets($theme) ?>images/logo.png" width="380" />
+              </div>
+              <div class="post">
+                <div class="btn-sign">
+                  <a href="" class="login-window"><img src="<?php echo $this->assets($theme) ?>images/botaoaluno.png" /></a>
+                </div>
+              </div>
         </header>
        <nav class="">
-            <div class="container">
-                <div class="navbar-header">
+            <div class="">
+                <div class="">
                 </div>
-                <div id="navbar" class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
+                <div id="navbar" class="">
+                    <ul class="">
                      <li class='active'><a href='<?php echo $this->createUrl() ?>'><span>Home</span></a></li>
                      <li><a href='<?php echo $this->createUrl('sobre') ?>'><span>Sobre</span></a></li>
                      <li><a href='#'><span>Aulas</span></a></li>
                      <li class='last'><a href='<?php echo $this->createUrl('professor')?>'><span>Professores</span></a></li>
                      <li><a href='#'><span>Serviços</span></a></li>
-                     <li class='active'><a href='<?php echo $this->createUrl('contato')?>'><span>Contatos</span></a></li>
+                     <li class=''><a href='<?php echo $this->createUrl('contato')?>'><span>Contatos</span></a></li>
                     </ul>
                 </div><!--/.nav-collapse -->
             </div>
         </nav>
 
-        <div class="container">
+        <div class="">
 
             <?php echo $content ?>
 
@@ -41,10 +45,8 @@
         <footer>
             
         </footer>
-
-        <script type="text/javascript" src="<?php echo $this->assets($theme) ?>js/jquery-1.10.1.min.js"></script>
+<script type="text/javascript" src="<?php echo $this->assets($theme) ?>js/jquery-1.10.1.min.js"></script>
 <script type="text/javascript" src="<?php echo $this->assets($theme) ?>js/coin-slider.min.js"></script>
-<script type="text/javascript" src="<?php echo $this->assets($theme) ?>js/loginmoal.js"></script>
 <script type="text/javascript">
   $(document).ready(function() {
     $('#coin-slider').coinslider({ width: 1000,height:400, navigation:true,effect:'rain', delay: 5000,hoverPause: true });
